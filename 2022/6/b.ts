4d4a5127de7df736baf583a705fc0927d6fb12ea
tonyaellie <tonya@tokia.dev>
2022-12-06 15:13:43 +0000

    feat: aoc 2022 day 6

---
+++ 2022/6/b.ts
@@ -0,0 +1,29 @@
+export const x = '';
+
+const input = await Deno.readTextFile('./input.txt');
+
+const buffer: string[] = [];
+
+let done = false;
+
+input.split('').forEach((char, i) => {
+  // if done, return
+  if (done) {
+    return;
+  }
+  // add char to buffer
+  buffer.push(char);
+  // if buffer is more than 4 chars long
+  if (buffer.length > 14) {
+    // remove first char from buffer
+    buffer.shift();
+  }
+  // if buffer is 4 chars long
+  if (buffer.length === 14) {
+    // if each char in buffer is different
+    if (new Set(buffer).size === 14) {
+      console.log(i + 1, buffer.join(''));
+      done = true;
+    }
+  }
+});
