e71e2da83a8fa8252d5a4ef52d12bd6505227403
tonyaellie <tonya@tokia.dev>
2022-12-08 08:27:13 +0000

    feat: aoc 2022 day 8

---
+++ 2022/8/a.ts
@@ -0,0 +1,43 @@
+export const x = '';
+
+const input = await Deno.readTextFile('./input.txt');
+
+// convert the input grid to a 2d array
+const grid: number[][] = input
+  .split('\n')
+  .map((row) => row.split('').map((char) => parseInt(char, 10)));
+
+// map the grid to a true/false array of whether the cell is a tree visible from the outside
+// a cell is only visible from the outside if it is the higher than all the cells in any
+// direction (top, bottom, left, right)
+const treeMap: boolean[][] = grid.map((row, y) => {
+  return row.map((cell, x) => {
+    // if the cell is on the edge its visible
+    if (y === 0 || y === grid.length - 1 || x === 0 || x === row.length - 1) {
+      return true;
+    }
+    // get lists of the cells in each direction
+    const top = grid
+      .map((row) => row[x])
+      .slice(0, y)
+      .some((cellT) => cellT >= cell);
+    const bottom = grid
+      .map((row) => row[x])
+      .slice(y + 1)
+      .some((cellB) => cellB >= cell);
+    const left = grid[y].slice(0, x).some((cellL) => cellL >= cell);
+    const right = grid[y].slice(x + 1).some((cellR) => cellR >= cell);
+    return !(top && bottom && left && right);
+  });
+});
+
+// map to a grid of 1 and 0 for easier debugging
+const gridMap = treeMap.map((row) => row.map((cell) => (cell ? 1 : 0)));
+
+// print the grid
+gridMap.forEach((row) => console.log(row.join('')));
+
+// count the number of trees visible from the outside
+const trees = treeMap.flat().filter((cell) => cell).length;
+
+console.log(trees);
---
+++ 2022/8/b.ts
@@ -0,0 +1,57 @@
+export const x = '';
+
+const input = await Deno.readTextFile('./input.txt');
+
+// 30373
+// 25512
+// 65332
+// 33549
+// 35390
+
+// convert the input grid to a 2d array
+const grid: number[][] = input
+  .split('\n')
+  .map((row) => row.split('').map((char) => parseInt(char, 10)));
+
+// calculate the scenic value of all cells, the scenic value is the number of trees
+// until the next taller tree (not including that tree) or the edge of the grid in each
+// direction (top, bottom, left, right) multiplied together
+const treeMap: number[][] = grid.map((row, y) => {
+  return row.map((cell, x) => {
+    // get lists of the cells in each direction
+    // and count the number of trees until the
+    // next taller tree (not including that tree)
+    const top = grid
+      .map((row) => row[x])
+      .slice(0, y)
+      .reverse();
+    const topScore =
+      top.findIndex((cellT) => cellT >= cell) === -1
+        ? top.length
+        : top.findIndex((cellT) => cellT >= cell) + 1;
+    const bottom = grid.map((row) => row[x]).slice(y + 1);
+    const bottomScore =
+      bottom.findIndex((cellB) => cellB >= cell) === -1
+        ? bottom.length
+        : bottom.findIndex((cellB) => cellB >= cell) + 1;
+    const left = grid[y].slice(0, x).reverse();
+    const leftScore =
+      left.findIndex((cellL) => cellL >= cell) === -1
+        ? left.length
+        : left.findIndex((cellL) => cellL >= cell) + 1;
+    const right = grid[y].slice(x + 1);
+    const rightScore =
+      right.findIndex((cellR) => cellR >= cell) === -1
+        ? right.length
+        : right.findIndex((cellR) => cellR >= cell) + 1;
+    return topScore * bottomScore * leftScore * rightScore;
+  });
+});
+
+// print the grid
+grid.forEach((row) => console.log(row.join('')));
+
+// find the cell with the highest scenic value
+const max = treeMap.flat().reduce((a, b) => Math.max(a, b));
+
+console.log(max);
